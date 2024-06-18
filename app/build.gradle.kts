34543a903540c45fc3b4388912eaae608dcbeac3
ZAMA\zonce <zonceya@gmail.com>
2024-06-19 00:12:47 +0200

    configure distributtion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,11 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id ("kotlin-kapt")
-    id ("dagger.hilt.android.plugin")
-    id("kotlin-android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -21,7 +23,12 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
+    val properties = Properties()
+    properties.load(project.rootProject.file("local.properties").inputStream())
+    val apiKey = properties.getProperty("API_KEY")
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -29,6 +36,9 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+        debug {
+            buildConfigField("String", "API_KEY", "\"$apiKey\"")
         }
     }
     kapt {
@@ -43,8 +53,20 @@ android {
     }
 }
 
-dependencies {
+tasks.register<Copy>("replaceGoogleServicesJson") {
+    val properties = Properties()
+    properties.load(project.rootProject.file("local.properties").inputStream())
+    val apiKey = properties.getProperty("API_KEY")
 
+    from("app/google-services.json.template")
+    into("app/")
+    rename { "google-services.json" }
+    filter { line -> line.replace("\${API_KEY}", apiKey) }
+}
+
+tasks.getByName("preBuild").dependsOn("replaceGoogleServicesJson")
+
+dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -53,38 +75,30 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation( "androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-common:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-    kapt ("androidx.room:room-compiler:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    // Navigation
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.7.7")
-    // Glide
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
-    kapt ("com.github.bumptech.glide:compiler:4.16.0")
-    //Dagger - Hilt
-    implementation ("com.google.dagger:hilt-android:2.50")
-    kapt ("com.google.dagger:hilt-android-compiler:2.50")
-    kapt ("androidx.hilt:hilt-compiler:1.2.0")
-    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
-    implementation ("com.google.dagger:hilt-android-gradle-plugin:2.50")
-    implementation ("androidx.navigation:navigation-compose:2.7.7")
-    // Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-   // implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.3")
-    //implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
-    // Gson
-    implementation ("com.google.code.gson:gson:2.10.1")
-    // Timber
-    implementation ("com.jakewharton.timber:timber:5.0.1")
-    //ViewPager
-    implementation ("androidx.viewpager2:viewpager2:1.0.0")
-    // Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    kapt("com.github.bumptech.glide:compiler:4.16.0")
+    implementation("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-android-compiler:2.50")
+    kapt("androidx.hilt:hilt-compiler:1.2.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation("com.google.dagger:hilt-android-gradle-plugin:2.50")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.jakewharton.timber:timber:5.0.1")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id ("com.android.library") version "7.1.0" apply false
     id ("com.google.dagger.hilt.android") version "2.50" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
